4f725d4bee9460c6dc7246d82bee616b8f16564b
Evan Morschhauser <evanmorsch@gmail.com>
2023-06-29 00:45:58 -0400

    Move Bullet class; Massively simplified bullet class

---
+++ gameobjs/Bullet.js
@@ -0,0 +1,15 @@
+const BULLET_LIFE = 1000      //in ms
+const BULLET_RADIUS = 2     //in px
+const BULLET_RESOLUTION = 3 //heightmap resolution for bullet
+
+class Bullet extends Entity {
+	constructor(pos, vel) {
+		super(pos, vel)
+		this.heightMap = new HeightMap(BULLET_RESOLUTION, BULLET_RADIUS)
+		this.active = true
+	}
+	update() {
+		super.update()
+		this.active = (Date.now() - this.birth_time) < BULLET_LIFE
+	}
+}
