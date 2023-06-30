893347cff746774f7ec632da5160e501a724b7c1
Evan Morschhauser <evanmorsch@gmail.com>
2023-06-30 00:11:05 -0400

    Update main for new file layout

---
+++ gameobjs/Helpers.js
@@ -0,0 +1,64 @@
+class Fade
+{
+	constructor(start)
+	{
+		this.start = start
+		this.curr = start
+	}
+	fade()
+	{
+		if (this.curr > 0) this.curr--
+	}
+	get amnt()
+	{
+		return this.curr
+	}
+	applyTo(r, g, b)
+	{
+		return "rgba("+(255*(this.curr/this.start))+", "+(255*(this.curr/this.start))+", "+(255*(this.curr/this.start))+", 1)"
+	}
+}
+
+class HeightMap
+{
+	constructor(resolution, start_height = 0)
+	{
+		this.map = new Array(resolution).fill(start_height)
+	}
+	randomize(min, max)
+	{
+		this.map.forEach(
+			function(a, b, c){
+				c[b] = Math.rand_range(min, max)
+			}
+		)
+	}
+	get_coords(i, offset = new _vector(0, 0), rot = 0) {
+		return new _vector(
+			offset.x + ( Math.cos( ( (Math.PI * 2) * (i / this.map.length) ) + rot ) * this.map[i] ), 
+			offset.y + ( Math.sin( ( (Math.PI * 2) * (i / this.map.length) ) + rot ) * this.map[i] )
+		)
+	}
+	to_particles(offset = new _vector(0, 0), rot = 0)
+	{
+		var ret_particles = []
+
+		for (let i=0;i<this.map.length;i++)
+		{
+			ret_particles.push(
+				new Fragment( this.get_coords(i, offset, rot), this.get_coords((i+1)%this.map.length, offset, rot), offset, 100 )
+			)
+		}
+		
+		return ret_particles
+	}
+	draw(offset = new _vector(0, 0), rot = 0)
+	{
+		ctx.beginPath()
+		this.map.forEach(function(a, b) {
+			b==0 ? ctx.moveTo(this.get_coords(b, offset, rot).x, this.get_coords(b, offset, rot).y) : ctx.lineTo(this.get_coords(b, offset, rot).x, this.get_coords(b, offset, rot).y)
+		}, this)
+		ctx.closePath()
+		ctx.stroke()
+	}
+}
