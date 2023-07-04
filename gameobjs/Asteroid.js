d4d795bc6aff76678354653058e0cb97fd16e8ba
Evan Morschhauser <evanmorsch@gmail.com>
2023-07-04 19:45:11 -0400

    Asteroid now uses defined min_parent_size

--- gameobjs/Asteroid.js
+++ gameobjs/Asteroid.js
@@ -5,6 +5,7 @@ const ASTEROID_MAX_START_SPEED = 1
 const ASTEROID_MIN_START_SPEED = 0.1
 const ASTEROID_MAX_START_RSPEED = 0.04
 const ASTEROID_CHILD_COUNT = 1
+const ASTEROID_MIN_PARENT_SIZE = 15//smallest px size of a child bearing asteroid
 //how far the velocity will deviate from aiming directly at the center of the screen
 const ASTEROID_START_DIR_DEVIATION = 0.35
 
@@ -22,15 +23,22 @@ class Asteroid extends Entity {
 
 		this.rot_vel = Math.rand_range(-ASTEROID_MAX_START_RSPEED, ASTEROID_MAX_START_RSPEED)
 
-		this.heightMap = new HeightMap(Math.floor(Math.rand_range(ASTEROID_RESOLUTION_MIN, ASTEROID_RESOLUTION_MAX)))
-		this.heightMap.randomize(this.radius*(1-ASTEROID_SCALE_DEVIATION), this.radius*(1+ASTEROID_SCALE_DEVIATION))
+		this.heightMap = new HeightMap(
+			Math.floor(
+				Math.rand_range(ASTEROID_RESOLUTION_MIN, ASTEROID_RESOLUTION_MAX)
+			)
+		)
+		this.heightMap.randomize(
+			this.radius*(1-ASTEROID_SCALE_DEVIATION),
+			this.radius*(1+ASTEROID_SCALE_DEVIATION)
+		)
 
         this.set_collision_mask(Asteroid, Particle)
 	}
 	collide(ent) {//spawn new asteroids if needed and kill the asteroid
 		super.collide(ent)
 		//create children
-		if ((this.MAXSIZE/15)>1)
+		if (this.size >= ASTEROID_MIN_PARENT_SIZE)
 		{
 			for (var i=0;i<ASTEROID_CHILD_COUNT;i++)
 			{
